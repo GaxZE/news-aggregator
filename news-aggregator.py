18ccf816ae1d8253cd2497d62c4ced38315ec274
Gary Hawes <gary.hawes@gmail.com>
2023-01-12 15:15:15 +0000

    Adding further functionality to add multiple queries

--- news-aggregator.py
+++ news-aggregator.py
@@ -3,19 +3,28 @@ from requests import get
 
 
 def get_articles(search):
-    url = f"https://news.google.com/rss/search?q={search}&hl=en-GB&gl=GB&ceid=GB:en"
-    xml = get(url)
-    parser = Parser(xml=xml.content, limit=10)
-    feed = parser.parse()
     news_list = []
+    for term in search:
+        url = f"https://news.google.com/rss/search?q={term}&hl=en-GB&gl=GB&ceid=GB:en"
+        xml = get(url)
+        parser = Parser(xml=xml.content, limit=5)
+        feed = parser.parse()
 
-    for item in feed.feed:
-        try:
-            news_list.append(item.title)
-        except:
-            pass
+        for item in feed.feed:
+            try:
+                news = {
+                    "title": item.title,
+                    "link": item.link,
+                    "date": item.publish_date
+                }
+                news_list.append(news)
+            except:
+                pass
     return news_list
 
 
-for title in get_articles("QPR"):
-    print(title)
+for article in get_articles(["QPR", "South Oxhey"]):
+    print(article["title"])
+    print(article["link"])
+    print(article["date"])
+    print("--")
